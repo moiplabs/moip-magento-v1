234deac6ca7b0baf3fbf9bd09ab4935b485ded5c
Bruno <brunoelisei@gmail.com>
2012-10-31 18:01:23 -0200

    :D

--- skin/frontend/default/default/o2ti_moip/js/formulario.js
+++ skin/frontend/default/default/o2ti_moip/js/formulario.js
@@ -5,13 +5,18 @@ cartao = function(){
 	if (!document.getElementById('shipping:firstname').value && !document.getElementById('billing:firstname').value)
 		{
  		document.getElementById('moip_o2preencha').style.display = 'block';
+ $MW_Onestepcheckout('#moip_o2preencha').hide();
+				 $MW_Onestepcheckout("#moip_o2preencha").show("slow");
 		document.getElementById('moip_boleto').style.display = 'none';
 		document.getElementById('moip_debito').style.display = 'none';
 		} else{
 		 document.getElementById('moip_credito').style.display = 'block';
+$MW_Onestepcheckout('#moip_credito').hide();
+				 $MW_Onestepcheckout("#moip_credito").show("slow");
 		 document.getElementById('moip_o2preencha').style.display = 'none';
 		 document.getElementById('moip_boleto').style.display = 'none';
 		 document.getElementById('moip_debito').style.display = 'none';
+$MW_Onestepcheckout('#advice-required-entry_payment_o2ti').remove();
 		}
 	if (!document.getElementById('shipping:firstname').value) {
  		document.getElementById('credito_portador_nome').value = document.getElementById('billing:firstname').value + ' ' + document.getElementById('billing:lastname').value;
@@ -23,23 +28,35 @@ cartao = function(){
  		document.getElementById('credito_portador_telefone').value = document.getElementById('shipping:telephone').value;
 }
   };
+
+
 boleto = function(){
 				document.getElementById('moip_debito').style.display='none';
                 document.getElementById('moip_o2preencha').style.display='none';
                 document.getElementById('moip_boleto').style.display='block';
+				$MW_Onestepcheckout('#moip_boleto').hide();
+				$MW_Onestepcheckout("#moip_boleto").show("slow");
                 document.getElementById('imagemcartao').style.opacity='0.4';
                 document.getElementById('imagemtrans').style.opacity='0.4';
                 document.getElementById('imageboleto').style.opacity='1';
-                document.getElementById('moip_credito').style.display='none'
+                document.getElementById('moip_credito').style.display='none';
+$MW_Onestepcheckout('#advice-required-entry_payment_o2ti').remove();
+
 				};
 transf = function(){
 				document.getElementById('moip_debito').style.display='block';
+				$MW_Onestepcheckout('#moip_debito').hide();
+				$MW_Onestepcheckout("#moip_debito").show("slow");
+				document.getElementById('moip_debito_bandeiras').style.display='block';
+				$MW_Onestepcheckout('#moip_debito_bandeiras').hide();
+				$MW_Onestepcheckout("#moip_debito_bandeiras").show("slow");
 				document.getElementById('moip_o2preencha').style.display='none';
 				document.getElementById('moip_boleto').style.display='none';
 				document.getElementById('moip_credito').style.display='none';
                 document.getElementById('imagemcartao').style.opacity='0.4';
                 document.getElementById('imagemtrans').style.opacity='1';
                 document.getElementById('imageboleto').style.opacity='0.4';
+$MW_Onestepcheckout('#advice-required-entry_payment_o2ti').remove();
 };
 
 bb = function(){
@@ -48,7 +65,11 @@ bb = function(){
                 document.getElementById('debbradesco').style.opacity='0.4';
                 document.getElementById('debitau').style.opacity='0.4';
                 document.getElementById('Banrisul').style.opacity='0.4';
-				document.getElementById('pagdebito').style.display='block';			
+				document.getElementById('pagdebito').style.display='block';
+				$MW_Onestepcheckout('#pagdebito').hide();
+				$MW_Onestepcheckout("#pagdebito").show("slow");
+document.getElementById('checkout-payment-banco').style.display='none';
+			
 };
 bradesco = function(){
 				document.getElementById('checkout-payment-o2ti-deb').style.display='none';
@@ -56,7 +77,10 @@ bradesco = function(){
                 document.getElementById('debbradesco').style.opacity='1';
                 document.getElementById('debitau').style.opacity='0.4';
                 document.getElementById('Banrisul').style.opacity='0.4';
-				document.getElementById('pagdebito').style.display='block';			
+				document.getElementById('pagdebito').style.display='block';	
+				$MW_Onestepcheckout('#pagdebito').hide();
+				$MW_Onestepcheckout("#pagdebito").show("slow");
+document.getElementById('checkout-payment-banco').style.display='none';
 };
 itau = function(){
 				document.getElementById('checkout-payment-o2ti-deb').style.display='none';
@@ -64,7 +88,10 @@ itau = function(){
                 document.getElementById('debbradesco').style.opacity='0.4';
                 document.getElementById('debitau').style.opacity='1';
                 document.getElementById('Banrisul').style.opacity='0.4';
-				document.getElementById('pagdebito').style.display='block';			
+				document.getElementById('pagdebito').style.display='block';	
+				$MW_Onestepcheckout('#pagdebito').hide();
+				$MW_Onestepcheckout("#pagdebito").show("slow");
+document.getElementById('checkout-payment-banco').style.display='none';
 };
 banrisul = function(){
 				document.getElementById('checkout-payment-o2ti-deb').style.display='none';
@@ -73,5 +100,64 @@ banrisul = function(){
                 document.getElementById('debitau').style.opacity='0.4';
                 document.getElementById('Banrisul').style.opacity='1';
 				document.getElementById('pagdebito').style.display='block';
+				$MW_Onestepcheckout('#pagdebito').hide();
+				$MW_Onestepcheckout("#pagdebito").show("slow");
+document.getElementById('checkout-payment-banco').style.display='none';
+};
+visa = function(){
+   document.getElementById('Visa').style.opacity='1';
+   document.getElementById('Mastercard').style.opacity='0.4';
+   document.getElementById('Diners').style.opacity='0.4';
+   document.getElementById('AmericanExpress').style.opacity='0.4';
+   document.getElementById('Hipercard').style.opacity='0.4';
+document.getElementById('checkout-payment-bandeira').style.display='none';
+
+};
+mastercard = function(){
+   document.getElementById('Visa').style.opacity='0.4';
+   document.getElementById('Mastercard').style.opacity='1';
+   document.getElementById('Diners').style.opacity='0.4';
+   document.getElementById('AmericanExpress').style.opacity='0.4';
+   document.getElementById('Hipercard').style.opacity='0.4';
+document.getElementById('checkout-payment-bandeira').style.display='none';
+};
+americanexpress = function(){
+   document.getElementById('Visa').style.opacity='0.4';
+   document.getElementById('Mastercard').style.opacity='0.4';
+   document.getElementById('Diners').style.opacity='0.4';
+   document.getElementById('AmericanExpress').style.opacity='1';
+   document.getElementById('Hipercard').style.opacity='0.4';
+document.getElementById('checkout-payment-bandeira').style.display='none';
 };
 
+diners = function(){
+   document.getElementById('Visa').style.opacity='0.4';
+   document.getElementById('Mastercard').style.opacity='0.4';
+   document.getElementById('Diners').style.opacity='1';
+   document.getElementById('AmericanExpress').style.opacity='0.4';
+   document.getElementById('Hipercard').style.opacity='0.4';
+document.getElementById('checkout-payment-bandeira').style.display='none';
+};
+
+hipercard = function(){
+   document.getElementById('Visa').style.opacity='0.4';
+   document.getElementById('Mastercard').style.opacity='0.4';
+   document.getElementById('Diners').style.opacity='0.4';
+   document.getElementById('AmericanExpress').style.opacity='0.4';
+   document.getElementById('Hipercard').style.opacity='1';
+document.getElementById('checkout-payment-bandeira').style.display='none';
+};
+alterar = function(){
+	document.getElementById('alterar').style.display='none';
+	document.getElementById('formcli').style.display='block';
+	document.getElementById('manter').style.display='block';
+	$MW_Onestepcheckout('#formcli').hide();
+	$MW_Onestepcheckout("#formcli").show("slow");
+};
+manter = function(){
+	document.getElementById('manter').style.display='none';
+	document.getElementById('formcli').style.display='none';
+	document.getElementById('alterar').style.display='block';
+	};
+
+ 
